5f25fdb7e269432700571748679b824cfff4f3a3
Dubeyvaishali001 <vaishalidubeyofficial@gmail.com>
2024-05-16 10:53:11 +0530

    test

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log("Dubey")
+let accountEmail="abhishek@gmail.com"
+let accountPassword="6523652"
+let accountCity="Bengalore"
+const accountState="KArnataka"
+
+
+console.log(accountCity);
+console.table([accountEmail,accountCity,accountPassword,accountState]);
