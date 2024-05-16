60725642878feb389c24b9064e4a69145761e423
Dubeyvaishali001 <vaishalidubeyofficial@gmail.com>
2024-05-16 11:17:40 +0530

    conversion of data

---
+++ test02.js
@@ -0,0 +1,17 @@
+// let score=true
+// console.log(typeof(score));
+// console.log(typeof score);
+
+// let valNumber=Number(score)
+// console.log(typeof valNumber);
+// console.log(valNumber)
+
+
+// let isLoggedIn =""
+// let booleanVal=Boolean(isLoggedIn)
+// console.log(booleanVal)
+let some=23;
+let stringnum= String(some)
+console.log(stringnum)
+console.log(some)
+console.log(typeof some)
